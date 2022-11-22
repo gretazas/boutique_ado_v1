0a3c6fd6fc981b400e75b0785fbb92e3d9aafe60
gretazas <esminis@yahoo.com>
2022-11-22 16:46:14 +0000

    Added sorting and product counts to producduct page

--- products/views.py
+++ products/views.py
@@ -6,7 +6,8 @@ from .models import Product, Category
 
 
 def all_products(request):
-    """ Show all products, plus sorting and search quaries """
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
@@ -27,7 +28,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -36,24 +37,22 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(render(
-                    request,
-                    "You didn't enter any search criteria!"
-                    ))
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-        context = {
-            'products': products,
-            'search_item': query,
-            'currant_categories': categories,
-            'current_sorting': f'{sort}_{direction}',
-        }
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
 
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ View individual product detail """
